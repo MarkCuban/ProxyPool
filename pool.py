707d1415de6e7cebf6da544ae44aa2f598b02e69
MarkCuban <86317539@qq.com>
2019-09-19 13:02:48 +0800

    Update pool.py

--- pool.py
+++ pool.py
@@ -1,2 +1,41 @@
 # -*- coding: utf-8 -*-
 
+import requests
+from lxml import etree
+
+
+    
+URLS = [
+    ''
+
+]
+
+
+
+class Proxy:
+
+    def __init__(self):
+        pass
+
+    def parse(self, url):
+        pass
+
+    def start_to_crawl(self):
+        pass
+
+
+    def get_from_xici(self):
+        pass
+
+
+    def get_from_xila(self):
+        pass
+
+    def get_from_kuai(self):
+        pass
+
+    def get_from_sixsix(self):
+        pass
+
+
+
